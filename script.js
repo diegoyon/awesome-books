4d89a423ec2081a8b431a337ef2ba8895ea2c1b4
ijeomaese <ijeomaodinko@gmail.com>
2022-06-27 18:58:14 -0400

    Add codes for storage

--- script.js
+++ script.js
@@ -1,56 +1,65 @@
+"use strict";
 // Array of books
 let collection = [];
-
-function addBook(title, author){
+console.log(localStorage.getItem("data"));
+if (localStorage.getItem("data") === null) {
+  collection = [];
+} else {
+  collection = JSON.parse(localStorage.getItem("data"));
+}
+function addBook(title, author) {
   let book = {};
   book.title = title;
   book.author = author;
   collection.push(book);
 }
 
-function removeBook(title){
-  collection = collection.filter(book => book.title !== title);
+function removeBook(title) {
+  collection = collection.filter((book) => book.title !== title);
 }
 
-const books = document.querySelector('.book-collection');
+const books = document.querySelector(".book-collection");
 
+const add = document.querySelector(".button");
 
-const add = document.querySelector('.button')
+const inputtitle = document.querySelector(".input-title");
+const inputauthor = document.querySelector(".input-author");
 
-const inputtitle = document.querySelector('.input-title');
-const inputauthor = document.querySelector('.input-author');
+add.addEventListener("click", addtocollection);
 
-add.addEventListener('click', addtocollection)
-
-function addtocollection(){
+function addtocollection() {
   addBook(inputtitle.value, inputauthor.value);
   refreshPage();
 }
 
-function remove(event){
+function remove(event) {
   removeBook(event.target.className);
   refreshPage();
 }
 
-function refreshPage(){
+function refreshPage() {
   books.innerHTML = "";
-  for(let i=0; i<collection.length; i++){
-    titledisplay = document.createElement('h2');
-    titledisplay.innerText = collection[i].title; 
-    authordisplay = document.createElement('h3');
-    authordisplay.innerText = collection[i].author; 
+  for (let i = 0; i < collection.length; i++) {
+    titledisplay = document.createElement("h2");
+    titledisplay.innerText = collection[i].title;
+    authordisplay = document.createElement("h3");
+    authordisplay.innerText = collection[i].author;
 
     books.appendChild(titledisplay);
     books.appendChild(authordisplay);
-    const buttonremove = document.createElement('button');
-    buttonremove.innerHTML = 'remove';
+    const buttonremove = document.createElement("button");
+    buttonremove.innerHTML = "remove";
     buttonremove.classList = collection[i].title;
-    buttonremove.addEventListener('click', remove);
+    buttonremove.addEventListener("click", remove);
     books.appendChild(buttonremove);
-    const line = document.createElement('hr');
+    const line = document.createElement("hr");
     books.appendChild(line);
   }
 
   inputtitle.value = "";
   inputauthor.value = "";
+  localStorage.setItem("data", JSON.stringify(collection));
 }
+//storage
+
+refreshPage();
