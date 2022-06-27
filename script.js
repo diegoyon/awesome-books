18083113e154635ac827fcba4db3baad205dfdf6
diego <diegoyon@outlook.com>
2022-06-27 15:53:03 -0600

    Implement add and remove button

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ function addBook(title, author){
 function removeBook(title){
   collection = collection.filter(book => book.title !== title);
 }
+
+const books = document.querySelector('.book-collection');
+
+
+const add = document.querySelector('.button')
+
+const inputtitle = document.querySelector('.input-title');
+const inputauthor = document.querySelector('.input-author');
+
+add.addEventListener('click', addtocollection)
+
+function addtocollection(){
+  addBook(inputtitle.value, inputauthor.value);
+  refreshPage();
+}
+
+function remove(event){
+  removeBook(event.target.className);
+  refreshPage();
+}
+
+function refreshPage(){
+  books.innerHTML = "";
+  for(let i=0; i<collection.length; i++){
+    titledisplay = document.createElement('h2');
+    titledisplay.innerText = collection[i].title; 
+    authordisplay = document.createElement('h3');
+    authordisplay.innerText = collection[i].author; 
+
+    books.appendChild(titledisplay);
+    books.appendChild(authordisplay);
+    const buttonremove = document.createElement('button');
+    buttonremove.innerHTML = 'remove';
+    buttonremove.classList = collection[i].title;
+    buttonremove.addEventListener('click', remove);
+    books.appendChild(buttonremove);
+    const line = document.createElement('hr');
+    books.appendChild(line);
+  }
+
+  inputtitle.value = "";
+  inputauthor.value = "";
+}
