078e99d43c983cfad208c2dc23c6fc376191da88
diego <diegoyon@outlook.com>
2022-06-27 22:25:57 -0600

    Set up linters

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-"use strict";
 // Array of books
 let collection;
 
-if (localStorage.getItem("data") === null) {
+const books = document.querySelector('.book-collection');
+
+const add = document.querySelector('.button');
+
+const inputtitle = document.querySelector('.input-title');
+const inputauthor = document.querySelector('.input-author');
+
+if (localStorage.getItem('data') === null) {
   collection = [];
 } else {
-  collection = JSON.parse(localStorage.getItem("data"));
+  collection = JSON.parse(localStorage.getItem('data'));
 }
 function addBook(title, author) {
-  let book = {};
+  const book = {};
   book.title = title;
   book.author = author;
   collection.push(book);
@@ -18,48 +24,62 @@ function removeBook(title) {
   collection = collection.filter((book) => book.title !== title);
 }
 
-const books = document.querySelector(".book-collection");
+function remove(event) {
+  removeBook(event.target.className);
+  books.innerHTML = '';
+  for (let i = 0; i < collection.length; i += 1) {
+    const titledisplay = document.createElement('h2');
+    titledisplay.innerText = collection[i].title;
+    const authordisplay = document.createElement('h3');
+    authordisplay.innerText = collection[i].author;
 
-const add = document.querySelector(".button");
+    books.appendChild(titledisplay);
+    books.appendChild(authordisplay);
+    const buttonremove = document.createElement('button');
+    buttonremove.innerHTML = 'remove';
+    buttonremove.classList = collection[i].title;
+    buttonremove.addEventListener('click', remove);
+    books.appendChild(buttonremove);
+    const line = document.createElement('hr');
+    books.appendChild(line);
+  }
 
-const inputtitle = document.querySelector(".input-title");
-const inputauthor = document.querySelector(".input-author");
+  inputtitle.value = '';
+  inputauthor.value = '';
+  localStorage.setItem('data', JSON.stringify(collection));
+}
 
-add.addEventListener("click", addtocollection);
+function refreshPage() {
+  books.innerHTML = '';
+  for (let i = 0; i < collection.length; i += 1) {
+    const titledisplay = document.createElement('h2');
+    titledisplay.innerText = collection[i].title;
+    const authordisplay = document.createElement('h3');
+    authordisplay.innerText = collection[i].author;
+
+    books.appendChild(titledisplay);
+    books.appendChild(authordisplay);
+    const buttonremove = document.createElement('button');
+    buttonremove.innerHTML = 'remove';
+    buttonremove.classList = collection[i].title;
+    buttonremove.addEventListener('click', remove);
+    books.appendChild(buttonremove);
+    const line = document.createElement('hr');
+    books.appendChild(line);
+  }
+
+  inputtitle.value = '';
+  inputauthor.value = '';
+  localStorage.setItem('data', JSON.stringify(collection));
+}
 
 function addtocollection() {
   addBook(inputtitle.value, inputauthor.value);
   refreshPage();
 }
 
-function remove(event) {
-  removeBook(event.target.className);
-  refreshPage();
-}
+add.addEventListener('click', addtocollection);
 
-function refreshPage() {
-  books.innerHTML = "";
-  for (let i = 0; i < collection.length; i++) {
-    const titledisplay = document.createElement("h2");
-    titledisplay.innerText = collection[i].title;
-    const authordisplay = document.createElement("h3");
-    authordisplay.innerText = collection[i].author;
-
-    books.appendChild(titledisplay);
-    books.appendChild(authordisplay);
-    const buttonremove = document.createElement("button");
-    buttonremove.innerHTML = "remove";
-    buttonremove.classList = collection[i].title;
-    buttonremove.addEventListener("click", remove);
-    books.appendChild(buttonremove);
-    const line = document.createElement("hr");
-    books.appendChild(line);
-  }
-
-  inputtitle.value = "";
-  inputauthor.value = "";
-  localStorage.setItem("data", JSON.stringify(collection));
-}
-//storage
+// storage
 
 refreshPage();
